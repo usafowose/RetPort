29819c57c469344b7244559c86b751d11b184956
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-08-25 23:43:03 -0400

    Setting up basic configuration for inquirer in console

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const quotes = require("./quotes");
 require("dotenv").config(); 
 
 // Key Config
-var keys = require("./Config/keys")
-
+var keys = require("./Config/keys");
+const InputPrompt = require('inquirer/lib/prompts/input');
+const { type } = require('os');
 var jobsKey = keys.jobsKey
 var newsKey = keys.newsKey; 
 
-console.log(jobsKey, newsKey); 
+// Set up inquirer for console queries and to capture inputs. 
+
+inquirer.prompt([ {
+    type: "input", 
+    message: "Welcome! What is your name", 
+    name: "Username"
+}
+]).then(answers => {
+    console.log(answers["Username"]);
+})
 
 
 
