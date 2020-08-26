da1ec7a44b6417688f1f53ad76f83c4dcc160a41
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-08-26 14:46:06 -0400

    Created a new Person class based on user input

--- app.js
+++ app.js
@@ -8,27 +8,46 @@ require("dotenv").config();
 var keys = require("./Config/keys");
 const InputPrompt = require('inquirer/lib/prompts/input');
 const { type } = require('os');
+const { stat } = require('fs');
 var jobsKey = keys.jobsKey
 var newsKey = keys.newsKey; 
 
-// Set up inquirer for console queries and to capture inputs. 
+// Set up inquirer for console queries and to capture inputs. ucfc
+class Person {
+    constructor(name, status) {
+        let person = {
+            name: name,
+            status: status
+        }; 
+        return person; 
+    }
+
+}; 
+
+
+
 let userData = []; 
 
 inquirer.prompt([ {
     type: "input", 
     message: "Welcome! What is your name", 
-    name: "Username"
+    name: "Username", 
+    
 }, {
     type: "checkbox", 
     message: "What do you classify yourself as?", 
-    choices: ["retired", "soon to retire"], 
+    choices: ["Retired", "Working, soon to retire", "Just Planning for the Future"], 
     name: "retirement_status"
 }
 ]).then(answers => {
    let userName = (answers["Username"]);
-   
+   let status = answers["retirement_status"][0]; 
 
+   let persObj = new Person(userName, status); 
+   userData.push(persObj); 
+   console.log(userData); 
 })
+
 
 
 
