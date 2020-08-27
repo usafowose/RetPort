82c6d32e0e6a0aa3b2a90fdd035bca74c109f026
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-08-27 11:33:41 -0400

    Adding more questions via inquirer to add to user obj

--- app.js
+++ app.js
@@ -1,51 +1,71 @@
 // Dependencies
-const axios = require('axios'); 
-const inquirer = require("inquirer"); 
-const quotes = require("./quotes"); 
-require("dotenv").config(); 
+const inquirer = require("inquirer");
+const quotes = require("./quotes");
 
-// Key Config
-var keys = require("./Config/keys");
 const InputPrompt = require('inquirer/lib/prompts/input');
 const { type } = require('os');
 const { stat } = require('fs');
+
+require("dotenv").config();
+const axios = require('axios');
+var keys = require("./Config/keys");
 var jobsKey = keys.jobsKey
-var newsKey = keys.newsKey; 
+var newsKey = keys.newsKey;
 
 // Set up inquirer for console queries and to capture inputs. ucfc
 class Person {
-    constructor(name, status) {
-        let person = {
-            name: name,
-            status: status
-        }; 
-        return person; 
+
+    constructor(name, status, currentCity, desiredMove) {
+        this.name = name;
+        this.status = status;
+        this.currentCity = currentCity;
+        this.desiredMove = desiredMove;
     }
+    createObj() {
+        let userObj = {
+            name: this.name,
+            status: this.status,
+            current_city: this.currentCity,
+            desiredMove: this.desiredMove
+        }
+        return userObj;
+    }
+};
 
-}; 
+let userData = [];
 
+inquirer.prompt([{
+    type: "input",
+    message: "Welcome! What is your name",
+    name: "Username"
 
-
-let userData = []; 
-
-inquirer.prompt([ {
-    type: "input", 
-    message: "Welcome! What is your name", 
-    name: "Username", 
-    
 }, {
-    type: "checkbox", 
-    message: "What do you classify yourself as?", 
-    choices: ["Retired", "Working, soon to retire", "Just Planning for the Future"], 
+    type: "checkbox",
+    message: "What do you classify yourself as?",
+    choices: ["Retired", "Working, Soon to Retire", "Just Planning for the Future"],
     name: "retirement_status"
+},
+{
+    type: "input",
+    message: "Where do you currently live? (city, ST)",
+    name: "current_city"
+},
+{
+    type: "input",
+    message: "Where have you thought of retiring? (Up to 3, separated by semicolons i.e. Chicago, IL; Achorage, AL)",
+    name: "destination_choics"
 }
 ]).then(answers => {
-   let userName = (answers["Username"]);
-   let status = answers["retirement_status"][0]; 
+    let userName = (answers["Username"]);
+    let status = answers["retirement_status"][0];
 
-   let persObj = new Person(userName, status); 
-   userData.push(persObj); 
-   console.log(userData); 
+    let current_city = answers["current_city"];
+    let dest_choices = answers["destination_choics"];
+
+
+    let persObj = new Person(userName, status, current_city, dest_choices);
+    userData.push(persObj.createObj())
+    console.log(userData);
 })
 
 
@@ -57,7 +77,7 @@ inquirer.prompt([ {
 // console.log(quotes); 
 
 
-module.exports = userData; 
+module.exports = userData;
 
 
 
