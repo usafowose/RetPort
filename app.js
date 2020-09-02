a7f2b180a4fca5e424fd7a4d7fc667bbe441f4a2
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-09-02 15:08:03 -0400

    Working Teleport API route git via axios

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
+// Requiring dependencies
 const axios = require('axios');
 const fs = require('fs');
 const keys = require("./Config/keys");
+const cTable = require('console.table');
 const { encode } = require('punycode');
+const quotes = require('./Model/quotes');
+
+// Importing API Keys from env for axios calls
+const jobsKey = keys.jobsKey
+const newsKey = keys.newsKey;
+
+// Randomly selecting quotes to display to user 
+let quoteNum = Math.floor(Math.random()*31); 
+let thisQuote = `quote${quoteNum}`;
+
+console.table(` \n --------------- \n ${quotes[thisQuote]}`); 
+
+// Sync File Read, JSON parsing,decoding and Assignment to Data Variable 
+let data = JSON.parse(
+    fs.readFileSync('./user.txt', { encoding: 'utf-8' })
+);
+
+var userName = data.name
+var userStatus = data.status; 
+var currentCity = data.current_city;
+var desiredMove = data.dest_choices; 
+
+// HTTP Get Requests via Axios Package to API's 
+
+axios.get('https://api.teleport.org/api/urban_areas/slug%3Aatlanta/scores').then(response => {
+    console.log(response.data)
+}).catch(err => console.log(err)); 
 
 
-// const inquire = require('./Model/inquire'); 
 
-// inquire(); 
-
-
-var jobsKey = keys.jobsKey
-var newsKey = keys.newsKey;
-
-var userData = [];
-
-// var stream = fs.readFile('./user.txt', { encoding: 'utf-8' }, (err, data) => {
-//     if (err) { console.log(err) }
-//     data = JSON.parse(data);
-//     userData.push(data)
-
-// });
-
-let data = fs.readFileSync('./user.txt', {encoding: 'utf-8'}); 
-data = JSON.parse(data);
-
-console.log(data); 
-
-
-//  console.log(...userData); 
 
 
 
