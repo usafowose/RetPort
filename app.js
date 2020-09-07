89d3e8ef62cbcd1d1d48bc0b592f9233cae429d7
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-09-07 12:44:31 -0400

    Adding personal markdown to gitignore and removing color properties of axios res*

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const inquire = require('inquirer');
 const jobsKey = keys.jobsKey
 const newsKey = keys.newsKey;
 
-console.log(keys)
 // Randomly selecting quotes to display to user 
 let quoteNum = Math.floor(Math.random() * 31);
 let thisQuote = `quote${quoteNum}`;
@@ -34,43 +33,49 @@ let desiredMove = data.desiredMove.toLowerCase().split(",")[0];
 // If city has two parts (i.e. Los Angeles, San Francisco, etc split and rejoin with hyphen "-")
 if (desiredMove.split(" ")[1]) {
     desiredMove = `${desiredMove.split(" ").join('-')}`;
-}
+};
 console.log(`Searching...${desiredMove}`);
 
 
 // HTTP Get Requests via Axios Package to TeleportAPI (Qual of Life Ratings) 
 axios.get(`https://api.teleport.org/api/urban_areas/slug:${desiredMove}/scores`).then(response => {
     let rawData = response.data.categories;
+    for (i = 0; i < rawData.length; i++) {
+        delete (rawData[i].color);
+    };
     console.table(rawData);
-    inquire.prompt({
-        type: "checkbox",
-        message: 'What is the most important quality of your retirement city?',
-        choices: ['Housing', 'Cost of Living', 'Startups', 'Venture Capital', 'Travel Connectivity', 'Commute', 'Business Freedom', 'Safety', 'Healthcare', 'Education', 'Environmental Quality', 'Economy', 'Taxation', 'Internet Access', 'Leisure & Culture', 'Tolerance', 'Outdoors'],
-        name: 'qualityOne'
-    }).then(answers => {
-        console.log(answers['qualityOne']);
-        for (let x in rawData) {
-            if (rawData[x].name === answers.qualityOne[0]) {
-                console.log(`\n In ${desiredMove.toUpperCase()}, This Is The Score In The ${rawData[x].name} Category: \n-------------------\n`)
-                console.table(rawData[x]);
-                break;
-            }
-        }
-        // console.log(rawData)
-    });
+
+
+    // console.table(rawData);
+    // inquire.prompt({
+    //     type: "checkbox",
+    //     message: 'What is the most important quality of your retirement city?',
+    //     choices: ['Housing', 'Cost of Living', 'Startups', 'Venture Capital', 'Travel Connectivity', 'Commute', 'Business Freedom', 'Safety', 'Healthcare', 'Education', 'Environmental Quality', 'Economy', 'Taxation', 'Internet Access', 'Leisure & Culture', 'Tolerance', 'Outdoors'],
+    //     name: 'qualityOne'
+    // }).then(answers => {
+    //     console.log(answers['qualityOne']);
+    //     for (let x in rawData) {
+    //         if (rawData[x].name === answers.qualityOne[0]) {
+    //             console.log(`\n In ${desiredMove.toUpperCase()}, This Is The Score In The ${rawData[x].name} Category: \n-------------------\n`)
+    //             console.table(rawData[x]);
+    //             break;
+    //         }
+    //     }
+    //     console.log(rawData)
+    // });
 }).catch(err => console.log(err));
 
 
 // HTTP Get Request for Local News
-// let query = "jobs"
+// let query = desiredMove
 // let targetURL = `https://content.guardianapis.com/search?q=${query}&api-key=${newsKey}`; 
 
-
 // axios.get(encodeURI(targetURL)).then(response => {
-//     console.log(response)
+//     var responseData = response.data.response.results;
+//     console.log(responseData);
 // }).catch(err => {
 //     err ? console.log(err) : console.log(`no error`); 
-// })
+// }); 
 
 
 // HTTP Get Req for Certain Jobs ***Use Inquirer?
