e7534b8408553ac352ecb875b9583bc681f091cb
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-09-07 22:25:51 -0400

    Wrapping axios calls in functions

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ let data = JSON.parse(
     fs.readFileSync('./user.txt', { encoding: 'utf-8' })
 );
 
-let userName = data.name
+let userName = data.name;
 let userStatus = data.status;
 let currentCity = data.current_city.toLowerCase();
 let desiredMove = data.desiredMove.toLowerCase().split(",")[0];
@@ -45,39 +45,36 @@ axios.get(`https://api.teleport.org/api/urban_areas/slug:${desiredMove}/scores`)
     };
     console.table(rawData);
 
-
-    // console.table(rawData);
-    // inquire.prompt({
-    //     type: "checkbox",
-    //     message: 'What is the most important quality of your retirement city?',
-    //     choices: ['Housing', 'Cost of Living', 'Startups', 'Venture Capital', 'Travel Connectivity', 'Commute', 'Business Freedom', 'Safety', 'Healthcare', 'Education', 'Environmental Quality', 'Economy', 'Taxation', 'Internet Access', 'Leisure & Culture', 'Tolerance', 'Outdoors'],
-    //     name: 'qualityOne'
-    // }).then(answers => {
-    //     console.log(answers['qualityOne']);
-    //     for (let x in rawData) {
-    //         if (rawData[x].name === answers.qualityOne[0]) {
-    //             console.log(`\n In ${desiredMove.toUpperCase()}, This Is The Score In The ${rawData[x].name} Category: \n-------------------\n`)
-    //             console.table(rawData[x]);
-    //             break;
-    //         }
-    //     }
-    //     console.log(rawData)
-    // });
+    inquire.prompt({
+        type: "checkbox",
+        message: 'What is the most important quality of your retirement city?',
+        choices: ['Housing', 'Cost of Living', 'Startups', 'Venture Capital', 'Travel Connectivity', 'Commute', 'Business Freedom', 'Safety', 'Healthcare', 'Education', 'Environmental Quality', 'Economy', 'Taxation', 'Internet Access', 'Leisure & Culture', 'Tolerance', 'Outdoors'],
+        name: 'qualityOne'
+    }).then(answers => {
+        console.log(answers['qualityOne']);
+        for (let x in rawData) {
+            if (rawData[x].name === answers.qualityOne[0]) {
+                console.log(`\n In ${desiredMove.toUpperCase()}, This Is The Score In The ${rawData[x].name} Category: \n-------------------\n`)
+                console.table(rawData[x]);
+                break;
+            }
+        }
+    });
 }).catch(err => console.log(err));
 
 
 // HTTP Get Request for Local News
-// let query = desiredMove
-// let targetURL = `https://content.guardianapis.com/search?q=${query}&api-key=${newsKey}`; 
+var getNews = () => {
+    let query = desiredMove;
+    let targetURL = `https://content.guardianapis.com/search?q=${query}&api-key=${newsKey}`; 
 
-// axios.get(encodeURI(targetURL)).then(response => {
-//     var responseData = response.data.response.results;
-//     console.log(responseData);
-// }).catch(err => {
-//     err ? console.log(err) : console.log(`no error`); 
-// }); 
-
-
+    axios.get(encodeURI(targetURL)).then(response => {
+        var responseData = response.data.response.results;
+        console.log(responseData);
+    }).catch(err => {
+        err ? console.log(err) : console.log(`no error`);
+    })
+};
 // HTTP Get Req for Certain Jobs ***Use Inquirer?
 
 
