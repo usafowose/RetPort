a9d517e7a0cc4e0527252a3ef482820fd0f515b4
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-09-23 21:26:45 -0400

    Required and called first axios req func export that depended on remaining

--- app.js
+++ app.js
@@ -2,112 +2,105 @@
 require('dotenv').config();
 
 const axios = require('axios');
-const fs = require('fs');
-const keys = require("./Config/keys");
-const cTable = require('console.table');
-const { encode } = require('punycode');
+const jobsKey = require("./Config/keys").jobsKey;
 const quotes = require('./Model/quotes');
-// const ask = require('./Model/inquire');
-const inquire = require('inquirer');
-const { Console } = require('console')
-const moment = require('moment');
-
-// Importing API Keys from env for axios calls
-const jobsKey = keys.jobsKey
-const newsKey = keys.newsKey;
+const data = require('./Requests/scores').data; 
+const getCity = require('./Requests/scores').getCity;
 
 // Randomly selecting quotes to display to user 
 let quoteNum = Math.floor(Math.random() * 31);
 let thisQuote = `quote${quoteNum}`;
 console.table(` \n --------------- \n ${quotes[thisQuote]} \n --------------- \n `);
 
+getCity();
+
 // Sync File Read, JSON parsing,decoding and Assignment to Data Variable 
-let data = JSON.parse(
-    fs.readFileSync('./user.txt', { encoding: 'utf-8' }));
+// let data = JSON.parse(
+//     fs.readFileSync('./user.txt', { encoding: 'utf-8' }));
 
 // Assigning data from txt file to Variables
-let userName = data.name;
-let userStatus = data.status;
-let currentCity = data.current_city.toLowerCase();
-let desiredMoveCity = data.desiredMove.trim().toLowerCase().split(",")[0];
-let desiredMoveState = data.desiredMove.trim().toUpperCase().split(",")[1];
-let jobNeeded = data.jobNeeded;
+// 
 
-console.log(desiredMoveState); 
+// getCity(); 
+
+
+// console.log(desiredMoveState); 
+
+
 // If city has two parts (i.e. Los Angeles, San Francisco, etc split and rejoin with hyphen "-")
-if (desiredMoveCity.split(" ")[1]) {
-    desiredMoveCity = `${desiredMoveCity.split(" ").join('-')}`;
-};
-console.log(`Searching...${desiredMoveCity} \n\n`);
+// if (desiredMoveCity.split(" ")[1]) {
+//     desiredMoveCity = `${desiredMoveCity.split(" ").join('-')}`;
+// };
+// console.log(`Searching...${desiredMoveCity} \n\n`);
 
 // HTTP Get Requests via Axios Package to TeleportAPI (Qual of Life Ratings) and filtering out the color
-var getCity = () => {
-    axios.get(`https://api.teleport.org/api/urban_areas/slug:${desiredMoveCity}/scores`).then(response => {
-        let rawData = response.data.categories;
-        for (i = 0; i < rawData.length; i++) {
-            delete (rawData[i].color);
-        };
-        console.table(rawData);
+// var getCity = () => {
+//     axios.get(`https://api.teleport.org/api/urban_areas/slug:${desiredMoveCity}/scores`).then(response => {
+//         let rawData = response.data.categories;
+//         for (i = 0; i < rawData.length; i++) {
+//             delete (rawData[i].color);
+//         };
+//         console.table(rawData);
 
-        // Using inquirer to see which of the scores is most important to user
-        inquire.prompt({
-            type: "checkbox",
-            message: 'What is the most important quality of your retirement city?',
-            choices: ['Housing', 'Cost of Living', 'Startups', 'Venture Capital', 'Travel Connectivity', 'Commute', 'Business Freedom', 'Safety', 'Healthcare', 'Education', 'Environmental Quality', 'Economy', 'Taxation', 'Internet Access', 'Leisure & Culture', 'Tolerance', 'Outdoors'],
-            name: 'qualityOne'
-        }).then(answers => {
-            for (let x in rawData) {
+//         // Using inquirer to see which of the scores is most important to user
+//         inquire.prompt({
+//             type: "checkbox",
+//             message: 'What is the most important quality of your retirement city?',
+//             choices: ['Housing', 'Cost of Living', 'Startups', 'Venture Capital', 'Travel Connectivity', 'Commute', 'Business Freedom', 'Safety', 'Healthcare', 'Education', 'Environmental Quality', 'Economy', 'Taxation', 'Internet Access', 'Leisure & Culture', 'Tolerance', 'Outdoors'],
+//             name: 'qualityOne'
+//         }).then(answers => {
+//             for (let x in rawData) {
 
-                if (rawData[x].name === answers.qualityOne[0]) {
+//                 if (rawData[x].name === answers.qualityOne[0]) {
 
-                    console.log(`\n In ${desiredMoveCity.toUpperCase()}, This Is The Score In The ${rawData[x].name} Category:\n-------------------\n`)
-                    console.table(rawData[x], '\n \n');
+//                     console.log(`\n In ${desiredMoveCity.toUpperCase()}, This Is The Score In The ${rawData[x].name} Category:\n-------------------\n`)
+//                     console.table(rawData[x], '\n \n');
 
-                    console.log(`Searching for news in ${desiredMoveCity} \n ---------------------- \n`)
-                    break;
+//                     console.log(`Searching for news in ${desiredMoveCity} \n ---------------------- \n`)
+//                     break;
 
-                };
-            }
-            // waiting 3 seconds to maxe axios call for news in selected city
-            setTimeout(() => getNews(), 3000)
+//                 };
+//             }
+//             // waiting 3 seconds to maxe axios call for news in selected city
+//             setTimeout(() => getNews(), 3000)
 
 
 
-        });
-    }).catch(err => console.log(err)
-    )
-};
+//         });
+//     }).catch(err => console.log(err)
+//     )
+// };
 
 
 
 // HTTP Get Request for Local News
-var getNews = () => {
-    let query = desiredMoveCity;
-    let targetURL = `https://content.guardianapis.com/search?q=${query}&page-size=30&order-by=relevance&api-key=${newsKey}&show-fields=shortUrl`;
+// var getNews = () => {
+//     let query = desiredMoveCity;
+//     let targetURL = `https://content.guardianapis.com/search?q=${query}&page-size=30&order-by=relevance&api-key=${newsKey}&show-fields=shortUrl`;
 
-    // Get Request
-    axios.get(encodeURI(targetURL)).then(response => {
-        var responseData = response.data.response.results;
-        // console.log(responseData); 
-        let userDataView = [];
+//     // Get Request
+//     axios.get(encodeURI(targetURL)).then(response => {
+//         var responseData = response.data.response.results;
+//         // console.log(responseData); 
+//         let userDataView = [];
 
-        // Filtering search results for News Articles as opposed to Sports, Music, etc. Also extracting only articleTitle and WebUrl for user to view and potentially read article. 
-        for (x in responseData) {
-            if (responseData[x].pillarName === 'News') {
-                let obj = {};
-                obj.articleName = responseData[x].webTitle;
-                obj.webUrl = responseData[x].fields.shortUrl;
-                obj.publicationDate = moment(responseData[x].webPublicationDate).format("MMM Do YYYY")
-                userDataView.push(obj);
-            }
-        }
-        console.table(userDataView);
+//         // Filtering search results for News Articles as opposed to Sports, Music, etc. Also extracting only articleTitle and WebUrl for user to view and potentially read article. 
+//         for (x in responseData) {
+//             if (responseData[x].pillarName === 'News') {
+//                 let obj = {};
+//                 obj.articleName = responseData[x].webTitle;
+//                 obj.webUrl = responseData[x].fields.shortUrl;
+//                 obj.publicationDate = moment(responseData[x].webPublicationDate).format("MMM Do YYYY")
+//                 userDataView.push(obj);
+//             }
+//         }
+//         console.table(userDataView);
 
-        if (jobNeeded) { setTimeout(() => getJobs(), 3000) };
-    }).catch(err => {
-        console.log(err)
-    })
-};
+//         if (jobNeeded) { setTimeout(() => getJobs(), 3000) };
+//     }).catch(err => {
+//         console.log(err)
+//     })
+// };
 
 var getJobs = () => {
     let jobsUrl = `https://data.usajobs.gov/api/search?LocationName=Baltimore, Maryland`
@@ -132,9 +125,9 @@ var getJobs = () => {
 
 }
 
-if (userName) {
-    getCity();
-}
+// if (userName) {
+//     getCity();
+// }
 // var getJobs = () => {};
 
 
